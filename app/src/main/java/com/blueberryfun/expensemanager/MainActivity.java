135f21fee014c3a39f53841e4efbb3dca08d0f57
Saloni <saloniagrawal.536@gmail.com>
2022-07-25 13:37:08 +0530

    saloni_21july

--- app/src/main/java/com/blueberryfun/expensemanager/MainActivity.java
+++ app/src/main/java/com/blueberryfun/expensemanager/MainActivity.java
@@ -1,19 +1,15 @@
 package com.blueberryfun.expensemanager;
 
 import android.content.Intent;
-
+import android.graphics.Color;
 import android.os.Bundle;
-
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
 import java.util.ArrayList;
-
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -21,8 +17,38 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().setElevation(0);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setElevation(0);
+        Button listButton = findViewById(R.id.listButton);
+        Button overviewbtn = findViewById(R.id.overviewButton);
+        listButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    Button btn = (Button) findViewById(R.id.listButton);
+                    btn.setBackground(getResources().getDrawable(R.drawable.round_button_white));
+                    ((Button)view).setTextColor(Color.parseColor("#0099ff"));
+                    btn.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_baseline_format_list_bulleted_primary, 0,0,0);
+                }
+                catch (Exception e){
+                    Log.e(TAG, "Error"+e);
+                }
+            }
+        });
+        overviewbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Button btn = (Button) findViewById(R.id.overviewButton);
+                    btn.setBackground(getResources().getDrawable(R.drawable.round_button_white));
+                    ((Button) view).setTextColor(Color.parseColor("#0099ff"));
+                    btn.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_baseline_piechart_primary, 0,0,0);
+                }
+                catch (Exception e){
+                    Log.e(TAG, "Error"+e);
+                }
+            }
+        });
         Log.d(TAG,"onCreate: Started.");
         ListView mlistview = (ListView) findViewById(R.id.listView);
         Adapters food = new Adapters(R.drawable.ic_food, "Food","Eggs and bread","24/02/2022","200");
